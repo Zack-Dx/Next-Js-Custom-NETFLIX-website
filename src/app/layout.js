73f1099a9b871061fe2344368f3f525961d855c8
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-04-30 15:16:13 +0530

    Footer and Responsive code added.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Mulish } from "next/font/google";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // Font Setup
 const mulish = Mulish({
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
